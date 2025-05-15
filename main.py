fecef41025fbca5ea31b56604301c55a252b10ef
SaberAttia1 <saber.attia.abdalwahed@gmail.com>
2025-05-15 07:36:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# program to automatically generate a PDA from a given CFG and simulate it on input strings.
+
 def simulate_pda(cfg, start_symbol, input_str):
     stack = [start_symbol]
     input_str += '$'
@@ -27,6 +29,7 @@ print(simulate_pda(cfg, "S", "aabb"))  # T
 print(simulate_pda(cfg, "S", "aaabbb"))  # T
 print(simulate_pda(cfg, "S", "aab"))  # F
 #========================================================
+# program to simulate a Turing Machine that increments a binary number by 1
 def increment_binary(bin_str):
     tape = list(bin_str)
     i = len(tape) - 1
@@ -43,3 +46,5 @@ def increment_binary(bin_str):
 
 print(increment_binary("1011"))  # => 1100
 print(increment_binary("111"))   # => 1000
+
+# id > 4490
